4ac0180ecb9be18b3c570557f1dea8c0a97c35a3
BobTheWho777 <ruslankirillov82@gmail.com>
2025-04-23 11:26:41 +0300

    Все контроллеры готовы

--- src/main/java/com/example/techpraktika/controller/SiteStageController.java
+++ src/main/java/com/example/techpraktika/controller/SiteStageController.java
@@ -1,4 +1,58 @@
 package com.example.techpraktika.controller;
 
+import com.example.techpraktika.entity.SiteStage;
+import com.example.techpraktika.service.SiteStageService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/stages")
 public class SiteStageController {
+    private final SiteStageService service;
+
+    public SiteStageController(SiteStageService service) {this.service = service;}
+
+    //Получить все стадии
+    @GetMapping
+    public List<SiteStage> getAllStage(){return service.findAll();}
+    //Получить стадию по ИД
+    @GetMapping("/{id}")
+    public ResponseEntity<SiteStage> getStageById(@PathVariable Long id){
+        Optional<SiteStage> stage = service.findById(id);
+        return stage.map(ResponseEntity::ok)
+                .orElseGet(()-> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+    //Создать новую стадию
+    @PostMapping
+    public ResponseEntity<SiteStage> createStage(@RequestBody SiteStage siteStage){
+        SiteStage savedStage = service.save(siteStage);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedStage);
+    }
+    //Обновить стадию по ИД
+    @PutMapping("/{id}")
+    public ResponseEntity<SiteStage> updateStage(@PathVariable Long id, @RequestBody SiteStage stage){
+        Optional<SiteStage> existingStage = service.findById(id);
+        if (existingStage.isPresent()){
+            stage.setId(id);
+            service.update(stage);
+            return  ResponseEntity.ok(stage);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    //Удалить стадию по ИД
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStage(@PathVariable Long id){
+        Optional<SiteStage> stage = service.findById(id);
+        if(stage.isPresent()){
+            service.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
--- src/main/java/com/example/techpraktika/controller/SupplierController.java
+++ src/main/java/com/example/techpraktika/controller/SupplierController.java
@@ -1,4 +1,60 @@
 package com.example.techpraktika.controller;
 
+import com.example.techpraktika.entity.Brigade;
+import com.example.techpraktika.entity.Supplier;
+import com.example.techpraktika.service.SupplierService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/suppliers")
 public class SupplierController {
+    private final SupplierService service;
+
+    public SupplierController(SupplierService service) {this.service = service;}
+
+    //Получить всех поставщиков
+    @GetMapping
+    public List<Supplier> getAllSuplier(){ return service.findAll();}
+    //Получить поставщика по ИД
+    @GetMapping("/{id}")
+    public ResponseEntity<Supplier> getSupllierById(@PathVariable Long id){
+        Optional<Supplier> supplier = service.findById(id);
+        return supplier.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+    //Создать нового поставщика
+    @PostMapping
+    public ResponseEntity<Supplier> createSupplier(@RequestBody Supplier supplier) {
+        Supplier savedSupplier = service.save(supplier);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedSupplier);
+    }
+    //Обновить поставщика по ИД
+    @PutMapping("/{id}")
+    public ResponseEntity<Supplier> updateSupplier(@PathVariable Long id, @RequestBody Supplier supplier) {
+        Optional<Supplier> existingSupplier = service.findById(id);
+        if (existingSupplier.isPresent()) {
+            supplier.setId(id);
+            service.update(supplier);
+            return ResponseEntity.ok(supplier);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    //Удалить поставщика по ИД
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSupplier(@PathVariable Long id) {
+        Optional<Supplier> supplier = service.findById(id);
+        if (supplier.isPresent()) {
+            service.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
 }
